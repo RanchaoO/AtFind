7833547a10712178306a07a0dc45e5a973f64b94
ranchaoou <ranchao088@gmail.com>
2021-06-21 01:19:10 +0100

    fixed package issues on widget_test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:at_find/main.dart';
+import 'package:at_tylers_demo/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
